02977e37ece9e5ce6e8543b7081a08a857045d4e
shail128 <shail1282003@gmail.com>
2026-02-12 21:59:05 +0530

    script.js

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function typing() {
   }
 }
 typing();
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
